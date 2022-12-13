9d7ba30bbdb565868a384aaf41ad7bc98148ceaf
parth <parthnmayani@gmail.com>
2022-12-13 08:12:19 +0530

    j

---
+++ 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/Selanium2/src/com/java/new1/Chrome.java
@@ -0,0 +1,13 @@
+package com.java.new1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Chrome {
+	public static void main(String[] args) {
+		System.setProperty("Webdriver.chrome.driver", "F:\\ggggg\\selenium-chromium-driver-4.7.0");
+		WebDriver driver= new ChromeDriver();
+		
+	}
+
+}
---
+++ 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/Selanium2/src/com/java/new1/New1.java
@@ -0,0 +1,11 @@
+package com.java.new1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.*;
+public class New1 {
+	public static void main(String[] args) {
+		
+	}
+
+}
